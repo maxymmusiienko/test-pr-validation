f88363d04774e1d02c1f7ad390382523589b70bc
maxymmusiienko <maxymvm@gmail.com>
2024-06-11 23:25:39 +0300

    test-gc

--- src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
+++ src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
@@ -1,12 +1,52 @@
 package net.broscorp.gcimpl;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GarbageCollectorImplementation implements GarbageCollector {
 
-  @Override
-  public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
-     return null;
-  }
-}
+    private Set<ApplicationBean> markedBeans;
 
+    {
+        markedBeans = new HashSet<>();
+    }
+
+    @Override
+    public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
+        markedBeans.clear();
+        stack.getStack().forEach(frame -> frame.getParameters().forEach(this::mark));
+
+        List<ApplicationBean> garbage = new ArrayList<>();
+        heap.getBeans().values().forEach(bean -> {
+            if (!markedBeans.contains(bean)) {
+                garbage.add(bean);
+            }
+        });
+
+        return garbage;
+    }
+
+    private void mark(ApplicationBean bean) {
+        if (bean == null || markedBeans.contains(bean)) {
+            return;
+        }
+
+        markedBeans.add(bean);
+        List<ApplicationBean> beansToMark = new ArrayList<>();
+        beansToMark.add(bean);
+        while (!beansToMark.isEmpty()) {
+            ApplicationBean beanToMark = beansToMark.remove(0);
+            beanToMark.getFieldValues().values().forEach(value -> {
+                if (value instanceof ApplicationBean) {
+                    ApplicationBean beanValue = (ApplicationBean) value;
+                    if (!markedBeans.contains(beanValue)) {
+                        markedBeans.add(beanValue);
+                        beansToMark.add(beanValue);
+                    }
+                }
+            });
+        }
+    }
+}
--- src/main/java/net/broscorp/gcimpl/HeapInfo.java
+++ src/main/java/net/broscorp/gcimpl/HeapInfo.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 public class HeapInfo {
 
   private Map<String, ApplicationBean> beans;
-
+  private int dmkladkla = 5;
   public HeapInfo(Map<String, ApplicationBean> beans) {
     this.beans = beans;
+    System.out.println("Changed wrong file.");
   }
 }
