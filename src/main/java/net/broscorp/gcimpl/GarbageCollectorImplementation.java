e327b991acbcda31111051b0396e7eba8846fb63
maxymmusiienko <maxymvm@gmail.com>
2024-06-14 16:44:42 +0300

    test validatorr gc task

--- src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
+++ src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
@@ -1,12 +1,39 @@
 package net.broscorp.gcimpl;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class GarbageCollectorImplementation implements GarbageCollector {
 
-  @Override
-  public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
-     return null;
-  }
-}
+    @Override
+    public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
+        List<ApplicationBean> unreachableObjects = new ArrayList<>();
+        Set<ApplicationBean> reachableObjects = new HashSet<>();
+        Deque<ApplicationBean> stackDeque = new LinkedList<>();
 
+        for (StackInfo.Frame frame : stack.getStack()) {
+            for (ApplicationBean parameter : frame.getParameters()) {
+                stackDeque.push(parameter);
+                while (!stackDeque.isEmpty()) {
+                    ApplicationBean currentBean = stackDeque.pop();
+                    if (currentBean != null && !reachableObjects.contains(currentBean)) {
+                        reachableObjects.add(currentBean);
+                        stackDeque.addAll(currentBean.getFieldValues().values());
+                    }
+                }
+            }
+        }
+
+        for (ApplicationBean bean : heap.getBeans().values()) {
+            if (!reachableObjects.contains(bean)) {
+                unreachableObjects.add(bean);
+            }
+        }
+
+        return unreachableObjects;
+    }
+}
